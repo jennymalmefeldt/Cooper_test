16d1a240e6d2007ffd7530c0ce125d073fdb6fbd
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-09-15 16:39:29 +0200

    Formatted a little.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,13 +11,9 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
-
   config.include FactoryBot::Syntax::Methods
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
