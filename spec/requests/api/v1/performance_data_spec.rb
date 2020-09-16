57f6e7f4932b46409ab611c054eae1d3194e0029
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-16 19:28:27 +0200

    Fixed typos in the performance_data_spec.rb

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -32,17 +32,17 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
               user: user)
       end
     end
-  end
-
-  before do
-    get '/api/v1/performance_data', headers: headers
-  end
-
-  it 'returns a 200 response status' do
-    expect(response).to have_http_status 200
-  end
-
-  it 'returns a collection of performance data' do
-    expect(response_json['entries'].count).to eq 5
+    
+    before do
+      get '/api/v1/performance_data', headers: headers
+    end
+  
+    it 'returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+  
+    it 'returns a collection of performance data' do
+      expect(response_json['entries'].count).to eq 5
+    end
   end
 end
