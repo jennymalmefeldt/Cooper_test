a831db2f936583e7d9801f301bbe0c6cfa98103e
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-16 17:19:04 +0200

    Modifying performance_data_controller.rb, with performance_data_params

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::PerformanceDataController < ApplicationController
   def create
-    data = PerformanceData.new(params[:performance_data])
-    it data.save
+   data = PerformanceData.new(performance_data_params)
+
+   if data.save
       head :ok
+    else
+      render json: { error: data.errors.full_messages }
     end
   end
+
+  private
+
+  def performance_data_params
+    params.require(:performance_data).permit!
+  end
+end
