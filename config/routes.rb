6610b590a1b7520ccc43533fc737dcbec60004d5
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-17 11:00:23 +0200

    Deleting files

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
+namespace :api do
+    resources :pings, only: [:index], constraints: { format: 'json '}
+    namespace :v1, defaults: { format: :json } do
+      resources :performance_data, only: [:create, :index]
+    end
+  end
+end
